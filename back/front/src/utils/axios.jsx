d8392833948f1c5cc95b90ea69a8d70c82f199ce
Siddharth Thakur <siddthakur06@gmail.com>
2026-09-02 15:44:42 +0530

    Fix deployed backend API URL

--- back/front/src/utils/axios.jsx
+++ back/front/src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://nutrilens-backend-0wkr.onrender.com",
 });
 
 export default instance;
